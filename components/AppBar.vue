c4fea01e9e3c53fa962cf2f1edfd20f283dc716f
Xiaodai Dai <diauweb@live.com>
2020-07-17 10:04:36 +0800

    feat: use layout and user icon

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="appbar">
-    <a class="logo font-medium text-xl text-black">EtherOJ</a>
-    <div class="links inline mx-8">
-      <nuxt-link to="/">
-        Home
-      </nuxt-link>
-      <nuxt-link to="/problems">
-        Problems
-      </nuxt-link>
-      <nuxt-link to="/submissions">
-        Submissions
-      </nuxt-link>
-      <nuxt-link to="/help">
-        Help
-      </nuxt-link>
-    </div>
+    <zi-row>
+      <zi-col class="flex items-center" span="4">
+        <a class="logo font-medium text-xl text-black">EtherOJ</a>
+      </zi-col>
+      <zi-col class="links flex items-center">
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+        <nuxt-link to="/problems">
+          Problems
+        </nuxt-link>
+        <nuxt-link to="/submissions">
+          Submissions
+        </nuxt-link>
+        <nuxt-link to="/help">
+          Help
+        </nuxt-link>
+      </zi-col>
+      <zi-col span="4">
+        <zi-user name="Devil Placeholder" />
+      </zi-col>
+    </zi-row>
   </div>
 </template>
 
 <style lang="postcss" scoped>
     .appbar {
-        @apply bg-white fixed w-full py-4 px-8 border-b-2 text-lg;
-        z-index: 100;
+      @apply bg-white fixed w-full py-3 px-8 border-b-2 text-lg;
+      z-index: 100;
     }
 
     .links a {

90472459f206a98ecb4bfcec0ea5b2140d30016d
Xiaodai Dai <diauweb@live.com>
2020-07-20 14:07:04 +0800

    fix: reduce line leading

--- components/Card.vue
+++ components/Card.vue
@@ -3,11 +3,17 @@
     <div class="-m-3 px-3 py-2 bg-gray-200 rounded-t-sm text-sm">
       <b>{{ name }}</b>
     </div>
-    <div class="pt-6">
+    <div class="pt-6 card-inner">
       <slot />
     </div>
   </div>
 </template>
+
+<style lang="postcss">
+.card-inner p {
+  @apply leading-normal m-0;
+}
+</style>
 
 <script>
 export default {
